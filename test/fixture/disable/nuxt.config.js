cffaf67e00a90b3a094a3e0d47cc67be2a3469ee
bryan <bryan.guner@gmail.com>
2021-01-10 04:35:45 -0500

    finally something works

--- test/fixture/disable/nuxt.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rootDir: __dirname,
-  buildModules: [
-    { handler: require('../../../') }
-  ],
-  netlifyFiles: {
-    copyExistingFiles: false
-  }
-}
--- test/fixture/warn/nuxt.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rootDir: __dirname,
-  buildModules: [
-    { handler: require('../../../') }
-       // With options
-       [ '@nuxtjs/netlify-files', {
-         /* module options */ } ]
-  ]
-}
